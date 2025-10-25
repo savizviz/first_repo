aad1a8804e2681244aa26ae6d00e0bf97fc15aa3
savizviz <savizarshiya@gmail.com>
2025-10-25 20:26:09 +0330

    add function

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include <string>
 using namespace std;
 
+void say_hello() {
+    cout << "Hello";
+}
+
 int main() {
     string name;
     cin >> name;
     cout << "hi" << name << endl;
+    say_hello();
     return 0;
 }
